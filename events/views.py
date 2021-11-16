ae6b93ab516c78f103f0f85c4bcb7629ca0b5371
unknown <ruedasjnthn@gmail.com>
2021-11-16 13:03:35 +0800

    form functionality

--- events/views.py
+++ events/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.core.mail import send_mail
+from django.http import HttpResponseRedirect
 from community_website.settings import EMAIL_HOST_USER
 from .forms import ComplaintForm
 
 def home(request):
     form = ComplaintForm()
+    submitted = False
     if request.method == 'POST':
         form = ComplaintForm(request.POST)
         subject = str(form['subject'].value())
@@ -14,6 +16,7 @@ def home(request):
         message = 'from ' + recepient + '\n' + address +'\n' + form_message
         if form.is_valid():
             form.save()
+            return HttpResponseRedirect('/?submitted=True')
         send_mail(
             subject, 
             message, 
@@ -21,7 +24,12 @@ def home(request):
             [EMAIL_HOST_USER], 
             fail_silently = False
             )
+    else:
+        form = ComplaintForm
+        if 'submitted' in request.GET:
+            submitted= True
 
     return render(request, 'home.html', {
-        'form': form
+        'form': form,
+        'submitted': submitted
         })
