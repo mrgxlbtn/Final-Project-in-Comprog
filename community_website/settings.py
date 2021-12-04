6d69542b389ea908243c479cb790ff69a9d3e10d
unknown <ruedasjnthn@gmail.com>
2021-12-04 10:49:53 +0800

    send mail

--- community_website/settings.py
+++ community_website/settings.py
@@ -129,7 +129,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'lgucomplaintsmanagementunit@gmail.com'
-EMAIL_HOST_PASSWORD = 'Comprog123'
+EMAIL_HOST_USER = 'jonathanruedas.dev@gmail.com'
+EMAIL_HOST_PASSWORD = 'jnthndev22'
 
 django_heroku.settings(locals())
